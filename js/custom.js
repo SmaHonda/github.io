567902e1f5fc37cccad4a78e5f934a5a1e8400e7
SmaHonda <86571238+SmaHonda@users.noreply.github.com>
2021-07-08 02:14:20 +0800

    0708

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+// $('#navPageCircle1').on("click",function(){
+//     console.log("ok")
+// })
+//中英日三種語言的富士山太陽隨機升起
+$('#sun').prop('src',`./img/1x/sun${Math.floor(Math.random()*3+1)}.png`)
+//navbar滑鼠滑入圓圈出現選單，離開選單則隱藏
+// $('#navPageCircle1').mouseenter(function(){
+//     $('#navPage1>button').fadeIn()
+// });
+// $('#navPage1>button').mouseout(function(){
+//     $('#navPage1>button').hide()
+// });
+// $('#navPageCircle2').mouseenter(function(){
+//     $('#navPage2>button').fadeIn()
+// });
+// $('#navPage2>button').mouseout(function(){
+//     $('#navPage2>button').hide()
+// });
+// $('#navPageCircle3').mouseenter(function(){
+//     $('#navPage3>button').fadeIn()
+// });
+// $('#navPage3>button').mouseout(function(){
+//     $('#navPage3>button').hide()
+// });
+// $('#navPageCircle4').mouseenter(function(){
+//     $('#navPage4>button').fadeIn()
+// });
+// $('#navPage4>button').mouseout(function(){
+//     $('#navPage4>button').hide()
+// });
+// $('#navPageCircle5').mouseenter(function(){
+//     $('#navPage5>button').fadeIn()
+// });
+// $('#navPage5>button').mouseout(function(){
+//     $('#navPage5>button').hide()
+// });
+
+
+// console.log($('#info').clientY);
+
+// $('#info').on('click',function(a){
+//     console.log(a.pageX);
+//     console.log(a.clientY);
+//     console.log($('section'))
+//     // $("h5").text(`${a.offsetX},${a.offsetY}`)
+// })
+
+
+
+//  離開頁頂時才出現 gotop 按扭
+// $("#navbar").fadeOut(0);
+    // $(window).scroll(function(){
+    //     if($(window).scroll() > 800){
+    //         $("#navbar").fadeIn(0);
+    //     }else{
+    //         $("#navbar").fadeOut(0);
+    //     }
+    // });
